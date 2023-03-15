debd5a89db6fbddae13d440cdfc3c4308f4f6c2d
Raghav Sharma <73748243+peregrine-falcon@users.noreply.github.com>
2023-03-15 17:15:48 +0530

    updatedFile

--- main/components/Featured/index.tsx
+++ main/components/Featured/index.tsx
@@ -83,7 +83,7 @@ export default class MultipleItems extends Component {
 
 
         return (
-            <div className="bg-bgblue py-20 marginFeature bg-featured">
+            <div className="bg-bgblue py-20 marginFeature bg-featured hidden lg:block">
                 <div className='mx-auto max-w-7xl sm:py-4 lg:px-8 '>
 
                     <div className="text-center pt-48 pb-10 md:pt-96">
--- main/pages/index.tsx
+++ main/pages/index.tsx
@@ -1,20 +1,20 @@
 import Head from 'next/head';
-import Navbar from './components/Navbar/index';
-import Banner from './components/Banner/index';
-import Aboutus from './components/Aboutus/index';
-import Dedicated from './components/Dedicated/index';
-import Digital from './components/Digital/index';
-import Beliefs from './components/Beliefs/index';
-import Wework from './components/Wework/index';
-import Ourteam from './components/Ourteam/index';
-import Featured from './components/Featured/index';
-import Manage from './components/Manage/index';
-import FAQ from './components/FAQ/index';
-import Testimonials from './components/Testimonials/index';
-import Articles from './components/Articles/index';
-import Joinus from './components/Joinus/index';
-import Insta from './components/Insta/index';
-import Footer from './components/Footer/index';
+import Navbar from '../components/Navbar/index';
+import Banner from '../components/Banner/index';
+import Aboutus from '../components/Aboutus/index';
+import Dedicated from '../components/Dedicated/index';
+import Digital from '../components/Digital/index';
+import Beliefs from '../components/Beliefs/index';
+import Wework from '../components/Wework/index';
+import Ourteam from '../components/Ourteam/index';
+import Featured from '../components/Featured/index';
+import Manage from '../components/Manage/index';
+import FAQ from '../components/FAQ/index';
+import Testimonials from '../components/Testimonials/index';
+import Articles from '../components/Articles/index';
+import Joinus from '../components/Joinus/index';
+import Insta from '../components/Insta/index';
+import Footer from '../components/Footer/index';
 
 
 
@@ -37,7 +37,7 @@ const Home = () => {
         <Beliefs />
         <Wework />
         <Ourteam />
-        {/* <Featured /> */}
+        <Featured />
         <Manage />
         <FAQ />
         <Testimonials />
